580382a80c02ca1fb952e34993146ef738a7d8c4
Ted Williams <tboxyz@gmail.com>
2019-09-08 21:13:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,11 @@ def reverse_each_word(sentence1)
   reversed_array = array_to_reverse.each{|word| word.reverse!}
   return reversed_array.join(" ")
 end
+
+def reverse_each_word(sentence2)
+  reversed_array = senttence2.split(" ")
+  reversed_array.collect {|word| word.reverse!}
+  reversed_array.join(" ")
+end
+
+  
